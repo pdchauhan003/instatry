e49bb727775481d7f29de3de895095adb52b55ee
pdchauhan003 <pdchauhan003@gmail.com>
2026-03-30 16:02:58 +0530

    update atlass url

--- Server/index.js
+++ Server/index.js
@@ -51,7 +51,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:3000",
-      "https://your-frontend-url.vercel.app"
+      "https://instagram-clone-3ntr.vercel.app/"
     ],
     methods: ["GET", "POST"],
     credentials: true
