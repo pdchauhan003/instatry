f86a309a1b8c6d12149103a4788e75c8c8b5b9db
pdchauhan003 <pdchauhan003@gmail.com>
2026-04-13 16:20:55 +0530

    update nodemailer

--- ecommercepro/app/api/auth/send-otp/route.js
+++ ecommercepro/app/api/auth/send-otp/route.js
@@ -1,40 +1,40 @@
-import {User} from '@/lib/database'
+import { User } from '@/lib/database'
 import { connectDB } from '@/lib/Connection';
-const nodemailer=require('nodemailer')
-export async function POST(req){
+const nodemailer = require('nodemailer')
+export async function POST(req) {
   await connectDB()
-    const {email} = await req.json();
-    console.log('email',email)
-    const otp=Math.floor(100000+Math.random()*900000).toString();
-    // const otp=String(otps)
-    const user=await User.findOneAndUpdate({email});
-    user.otp=otp
-    await user.save()
+  const { email } = await req.json();
+  console.log('email', email)
+  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+  // const otp=String(otps)
+  const user = await User.findOneAndUpdate({ email });
+  user.otp = otp
+  await user.save()
 
-    const transporter = nodemailer.createTransport({
+  const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false, 
+    secure: false,
     auth: {
-        user: 'prathamcd003@gmail.com',         
-        pass: 'shxo pbzd gknp msxr',      
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
-    });
+  });
 
-    const mailOptions = {
-      from: 'prathamcd003@gmail.com',
-      to: email,
-      subject: "Your OTP Code",
-      text: `Your OTP is: ${otp}.`,
-    };
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "Your OTP Code",
+    text: `Your OTP is: ${otp}.`,
+  };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log('mail error:',error);
-        return res.json({ status: "error", message: "Email sending failed" });
-      }
-    });
-    // return Response.json({ status: "ok", message: "OTP sent to your email" });
-    console.log('Otp sended...',otp)
-    return Response.json({success:true});
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log('mail error:', error);
+      return Response.json({ status: "error", message: "Email sending failed" });
+    }
+  });
+  // return Response.json({ status: "ok", message: "OTP sent to your email" });
+  console.log('Otp sended...', otp)
+  return Response.json({ success: true });
 }
